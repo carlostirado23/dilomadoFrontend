0ef57ccdde5e4f66e122c59c09cbde6fb4e31802
Carlos Tirado <mariotiradotovar@outlook.com>
2024-07-03 15:51:48 -0500

    unit test y mapas

---
+++ clase-12/library-project/src/AppRouter.jsx
@@ -0,0 +1,42 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
+import { HomePage } from './pages/HomePage';
+import { BookDetailPage } from './pages/BookDetailPage';
+import { SignupPage } from './pages/SignupPage/SignupPage';
+import { ROUTES } from './utils/routes';
+import { MapPage } from './pages/MapPage';
+// import { ChatPage } from './pages/ChatPage';
+
+const PublicRoutes = ({ children }) => {
+  const isLogged = localStorage.getItem('jwt');
+  return !isLogged ? children
+    : <Navigate to={ROUTES.HOME} />;
+};
+
+const PrivateRoute = ({children}) => {
+  const isLogged = localStorage.getItem('jwt');
+
+  return isLogged ? 
+    <Routes>
+      { children }
+      <Route path='/*' element={ <Navigate to={ROUTES.HOME} /> }></Route>
+    </Routes>
+    : <Navigate to={ROUTES.LOGIN} />
+}
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path={ROUTES.LOGIN} element={<PublicRoutes><LoginPage/></PublicRoutes>}></Route>
+      <Route path={ROUTES.SIGNUP} element={<PublicRoutes><SignupPage /></PublicRoutes>}></Route>
+      <Route path='/*' element={
+        <PrivateRoute>
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.DETAIL} element={<BookDetailPage />} />
+          <Route path={ROUTES.CHAT} element={<></>} />
+          <Route path={ROUTES.MAP} element={<MapPage />} />
+        </PrivateRoute>
+      }></Route>
+    </Routes>
+  )
+}
---
+++ clase-12/library-project/src/utils/routes.js
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  HOME: '/home',
+  DETAIL: '/book/:id',
+  CHAT: '/chat',
+  MAP: '/map',
+}
