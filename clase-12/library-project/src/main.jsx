329de30562445baf55a478ea4eaa68291161a54e
Carlos Tirado <mariotiradotovar@outlook.com>
2024-07-05 19:28:59 -0500

    faltan react router dom

--- clase-12/library-project/src/main.jsx
+++ clase-12/library-project/src/main.jsx
@@ -1,9 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import { AppRouter } from './AppRouter.jsx';
-import { AppContext } from './context/AppContext.jsx';
+import ReactDOM from "react-dom/client";
+import { AppRouter } from "./AppRouter.jsx";
+import { AppContext } from "./context/AppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <AppContext>
-    <AppRouter />
-  </AppContext>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <AppContext>
+        <AppRouter />
+    </AppContext>
+);
+
+// falta react router dom
